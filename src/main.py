fdcd45d87815796427b7ab65f67a35e8dafbd4d6
James <123058@combertonvc.org>
2025-11-28 08:45:13 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,354 +9,115 @@ t.hideturtle()
 log = []
 
 def addToLog(text):
-    # Create public
-    if (text):
+    if text:
         log.append(text)
-        # Print the last log
         print(text)
 
 def printLog():
     print(log)
 
-# Letters
+# Letters and Space Dictionary
+letters = {
+    'a': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x + 20, y + 100), t.goto(x + 40, y),
+                        t.penup(), t.goto(x + 10, y + 50), t.pendown(), t.goto(x + 30, y + 50)),
+    'b': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 40, y + 75),
+                        t.goto(x + 5, y + 50), t.goto(x + 40, y + 25), t.goto(x + 40, y), t.goto(x, y)),
+    'c': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x + 40, y), t.goto(x, y), 
+                        t.goto(x, y + 100), t.goto(x + 40, y + 100)),
+    'd': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 40, y + 75), 
+                        t.goto(x + 40, y), t.goto(x, y)),
+    'e': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x + 40, y), t.goto(x, y), 
+                        t.goto(x, y + 35), t.goto(x + 30, y + 35), t.goto(x, y + 35), t.goto(x, y + 100), 
+                        t.goto(x + 40, y + 100)),
+    'f': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 75), t.goto(x + 35, y + 75), 
+                        t.goto(x, y + 75), t.goto(x, y + 100), t.goto(x + 40, y + 100)),
+    'g': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 40, y + 100), 
+                        t.goto(x, y + 100), t.goto(x, y), t.goto(x + 40, y), t.goto(x + 40, y + 35), 
+                        t.goto(x + 20, y + 35)),
+    'h': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x, y + 50), 
+                        t.goto(x + 40, y + 50), t.goto(x + 40, y), t.goto(x + 40, y + 100)),
+    'i': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x + 40, y), t.goto(x + 20, y), 
+                        t.goto(x + 20, y + 100), t.goto(x, y + 100), t.goto(x + 40, y + 100)),
+    'j': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 35), t.goto(x, y), 
+                        t.goto(x + 40, y), t.goto(x + 40, y + 100), t.goto(x, y + 100)),
+    'k': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x, y + 50), 
+                        t.goto(x + 40, y + 100), t.goto(x, y + 50), t.goto(x + 40, y)),
+    'l': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x, y), 
+                        t.goto(x + 40, y)),
+    'm': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 20, y + 50), 
+                        t.goto(x + 40, y + 100), t.goto(x + 40, y)),
+    'n': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 40, y), 
+                        t.goto(x + 40, y + 100)),
+    'o': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 40, y + 100), 
+                        t.goto(x + 40, y), t.goto(x, y)),
+    'p': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 40, y + 100), 
+                        t.goto(x + 40, y + 60), t.goto(x, y + 60)),
+    'q': lambda x, y: (t.penup(), t.goto(x + 40, y), t.pendown(), t.goto(x + 40, y + 100), t.goto(x, y + 100), 
+                        t.goto(x, y + 60), t.goto(x + 40, y + 60)),
+    'r': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x + 40, y + 100), 
+                        t.goto(x + 40, y + 50), t.goto(x, y + 50), t.goto(x + 40, y)),
+    's': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x + 40, y), t.goto(x + 40, y + 50), 
+                        t.goto(x, y + 50), t.goto(x, y + 100), t.goto(x + 40, y + 100)),
+    't': lambda x, y: (t.penup(), t.goto(x + 20, y), t.pendown(), t.goto(x + 20, y + 100), t.goto(x, y + 100), 
+                        t.goto(x + 40, y + 100)),
+    'u': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 100), t.goto(x, y), 
+                        t.goto(x + 40, y), t.goto(x + 40, y + 100)),
+    'v': lambda x, y: (t.penup(), t.goto(x, y + 100), t.pendown(), t.goto(x + 20, y), t.goto(x + 40, y + 100)),
+    'w': lambda x, y: (t.penup(), t.goto(x, y + 100), t.pendown(), t.goto(x + 10, y), t.goto(x + 20, y + 50), 
+                        t.goto(x + 30, y), t.goto(x + 40, y + 100)),
+    'x': lambda x, y: (t.penup(), t.goto(x, y + 100), t.pendown(), t.goto(x + 40, y), t.goto(x + 20, y + 50), 
+                        t.goto(x, y), t.goto(x + 40, y + 100)),
+    'y': lambda x, y: (t.penup(), t.goto(x + 20, y), t.pendown(), t.goto(x + 20, y + 50), t.goto(x, y + 100), 
+                        t.goto(x + 20, y + 50), t.goto(x + 40, y + 100)),
+    'z': lambda x, y: (t.penup(), t.goto(x, y + 100), t.pendown(), t.goto(x + 40, y + 100), t.goto(x, y), 
+                        t.goto(x + 40, y)),
+    ' ': lambda x, y: (x + 50),  # Adding a space function
+}
 
-def letter_a(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x+20, y+100)
-    t.goto(x+40, y)
-    t.penup()
-    t.goto(x+10, y+50)
-    t.pendown()
-    t.goto(x+30, y+50)
-
-def letter_b(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+40, y+75)
-    t.goto(x+5, y+50)
-    t.goto(x+40, y+25)
-    t.goto(x+40, y)
-    t.goto(x, y)
-
-def letter_c(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x+40, y)
-    t.goto(x, y)
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    
-def letter_d(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+40, y+75)
-    t.goto(x+40, y)
-    t.goto(x, y)
-
-def letter_e(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x+40, y)
-    t.goto(x, y)
-    t.goto(x, y+35)
-    t.goto(x+30, y+35)
-    t.goto(x, y+35)
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    
-def letter_f(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+75)
-    t.goto(x+35, y+75)
-    t.goto(x, y+75)
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    
-def letter_g(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    t.goto(x, y+100)
-    t.goto(x, y)
-    t.goto(x+40, y)
-    t.goto(x+40, y+35)
-    t.goto(x+20, y+35)
-    
-def letter_h(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x, y+50)
-    t.goto(x+40, y+50)
-    t.goto(x+40, y)
-    t.goto(x+40, y+100)
-    
-def letter_i(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x+40, y)
-    t.goto(x+20, y)
-    t.goto(x+20, y+100)
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    
-def letter_j(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+35)
-    t.goto(x, y)
-    t.goto(x+40, y)
-    t.goto(x+40, y+100)
-    t.goto(x, y+100)
-    
-def letter_k(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x, y+50)
-    t.goto(x+40, y+100)
-    t.goto(x, y+50)
-    t.goto(x+40, y)
-
-def letter_l(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x, y)
-    t.goto(x+40, y)
-
-def letter_m(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+20, y+50)
-    t.goto(x+40, y+100)
-    t.goto(x+40, y)
-
-def letter_n(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+40, y)
-    t.goto(x+40, y+100)
-
-def letter_o(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    t.goto(x+40, y)
-    t.goto(x, y)
-
-def letter_p(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    t.goto(x+40, y+60)
-    t.goto(x, y+60)
-
-def letter_q(x, y):
-    t.penup()
-    t.goto(x+40, y)
-    t.pendown()
-    t.goto(x+40, y+100)
-    t.goto(x, y+100)
-    t.goto(x, y+60)
-    t.goto(x+40, y+60)
-
-def letter_r(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    t.goto(x+40, y+50)
-    t.goto(x, y+50)
-    t.goto(x+40, y)
-
-def letter_s(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x+40, y)
-    t.goto(x+40, y+50)
-    t.goto(x, y+50)
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-
-def letter_t(x, y):
-    t.penup()
-    t.goto(x+20, y)
-    t.pendown()
-    t.goto(x+20, y+100)
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-
-def letter_u(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x, y)
-    t.goto(x+40, y)
-    t.goto(x+40, y+100)
-    
-def letter_v(x, y):
-    t.penup()
-    t.goto(x, y+100)
-    t.pendown()
-    t.goto(x+20, y)
-    t.goto(x+40, y+100)
-    
-def letter_w(x, y):
-    t.penup()
-    t.goto(x, y+100)
-    t.pendown()
-    t.goto(x+10, y)
-    t.goto(x+20, y+50)
-    t.goto(x+30, y)
-    t.goto(x+40, y+100)
-    
-def letter_x(x, y):
-    t.penup()
-    t.goto(x, y+100)
-    t.pendown()
-    t.goto(x+40, y)
-    t.goto(x+20, y+50)
-    t.goto(x, y)
-    t.goto(x+40, y+100)
-    
-def letter_y(x, y):
-    t.penup()
-    t.goto(x+20, y)
-    t.pendown()
-    t.goto(x+20, y+50)
-    t.goto(x, y+100)
-    t.goto(x+20, y+50)
-    t.goto(x+40, y+100)
-    
-def letter_z(x, y):
-    t.penup()
-    t.goto(x, y+100)
-    t.pendown()
-    t.goto(x+40, y+100)
-    t.goto(x, y)
-    t.goto(x+40, y)
-    
 # Special Characters    
 
-def letter_1(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+5)
-    t.goto(x+5, y+5)
-    t.goto(x+5, y)
-    t.goto(x, y)
-    
-def letter_2(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+5)
-    t.goto(x+5, y+5)
-    t.goto(x+5, y)
-    t.goto(x, y)
-    t.penup()
-    t.goto(x, y+10)
-    t.pendown()
-    t.goto(x, y+100)
-    t.goto(x+5, y+100)
-    t.goto(x+5, y+10)
-    t.goto(x, y+10)
-
-def letter_3(x, y):
-    t.penup()
-    t.goto(x+5, y)
-    t.pendown()
-    t.goto(x+5, y+5)
-    t.goto(x+10, y+5)
-    t.goto(x+10, y-10)
-    t.goto(x, y-10)
-    t.goto(x, y-5)
-    t.goto(x+5, y-5)
-    t.goto(x+5, y)
-
-def letter_4(x, y):
-    t.penup()
-    t.goto(x+17.5, y+100)
-    t.pendown()
-    t.goto(x+22.5, y+100)
-    t.goto(x+22.5, y+90)
-    t.goto(x+17.5, y+90)
-    t.goto(x+17.5, y+100)
-
-def letter_5(x, y):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.goto(x, y+5)
-    t.goto(x+5, y+5)
-    t.goto(x+5, y)
-    t.goto(x, y)
-    t.penup()
-    t.goto(x, y+10)
-    t.pendown()
-    t.goto(x, y+50)
-    t.goto(x+35, y+50)
-    t.goto(x+35, y+95)
-    t.goto(x, y+95)
-    t.goto(x, y+100)
-    t.goto(x+40, y+100)
-    t.goto(x+40, y+45)
-    t.goto(x+5, y+45)
-    t.goto(x+5, y+10)
-    t.goto(x, y+10)
-
-
+special_characters = {
+    '1': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 5), t.goto(x + 5, y + 5), 
+                        t.goto(x + 5, y), t.goto(x, y)),
+    '2': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 5), t.goto(x + 5, y + 5), 
+                        t.goto(x + 5, y), t.goto(x, y), t.penup(), t.goto(x, y + 10), 
+                        t.pendown(), t.goto(x, y + 100), t.goto(x + 5, y + 100), 
+                        t.goto(x + 5, y + 10), t.goto(x, y + 10)),
+    '3': lambda x, y: (t.penup(), t.goto(x + 5, y), t.pendown(), t.goto(x + 5, y + 5), t.goto(x + 10, y + 5), 
+                        t.goto(x + 10, y - 10), t.goto(x, y - 10), t.goto(x, y - 5), 
+                        t.goto(x + 5, y - 5), t.goto(x + 5, y)),
+    '4': lambda x, y: (t.penup(), t.goto(x + 17.5, y + 100), t.pendown(), t.goto(x + 22.5, y + 100), 
+                        t.goto(x + 22.5, y + 90), t.goto(x + 17.5, y + 90), t.goto(x + 17.5, y + 100)),
+    '5': lambda x, y: (t.penup(), t.goto(x, y), t.pendown(), t.goto(x, y + 5), t.goto(x + 5, y + 5), 
+                        t.goto(x + 5, y), t.goto(x, y), t.penup(), t.goto(x, y + 10), 
+                        t.pendown(), t.goto(x, y + 50), t.goto(x + 35, y + 50), 
+                        t.goto(x + 35, y + 95), t.goto(x, y + 95), t.goto(x, y + 100), 
+                        t.goto(x + 40, y + 100), t.goto(x + 40, y + 45), t.goto(x + 5, y + 45), 
+                        t.goto(x + 5, y + 10), t.goto(x, y + 10)),
+}
 
 def draw_text(text, start_x, start_y, spacing=50, line_height=150):
     # Special Characters
     sc_text = text.replace(".", "1").replace("!", "2").replace(",", "3").replace("'", "4").replace("?", "5")
-    # Start
-    x = start_x
-    y = start_y
-    # For each letter in the text
+    x = start_x # Set the first x to the start_x
+    y = start_y # Set the first y to the start_y
     for letter in sc_text.lower():
         if letter == '\n':
             y -= line_height
             x = start_x
-        else:
-            func_name = 'letter_' + letter
-            letter_func = globals().get(func_name)
-            if letter_func:
-                letter_func(x, y)
-            x += spacing
+        elif letter in letters:
+            func = letters[letter]
+            if letter == ' ':
+                x += spacing # Add spacing with no letter
+            else:
+                func(x, y)
+                x += spacing # Add spacing after letter
+        elif letter in special_characters:
+            special_characters[letter](x, y)
+            x += spacing # Add spacing after special character
 
 def main():
-    text = "This is sample\ntext!\nSpecial Chars .!,?"
+    text = "This is\nsample\ntext!\n"
     addToLog("\n\nRunning...")
     draw_text(text, -200, 100, spacing=50)
 
